b6b8dbfe487de5a52dd55ad4770d8350ce40d0d3
anec-97 <anechdz22@gmail.com>
2024-09-19 18:31:54 -0600

    avance 20240919

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,19 +1,19 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Task } from './task.model';
+import { CardComponent } from "../../shared/card/card.component";
 
-interface Task {
-  id: string;
-  userId: string;
-  title: string;
-  summary: string;
-  dueDate: string;
-}
 @Component({
   selector: 'app-task',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './task.component.html',
   styleUrl: './task.component.css'
 })
 export class TaskComponent {
   @Input({ required: true }) task!: Task;
+  @Output() complete = new EventEmitter <string>();
+
+  onCompleteTask() {
+    this.complete.emit(this.task.id);
+  }
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,19 +1,18 @@
 import {Component, computed, EventEmitter, input, Input, Output, output} from '@angular/core';
+import {User} from './user.model';
+import { CardComponent } from "../shared/card/card.component";	
 
-interface User {
-  id: string;
-  name: string;
-  avatar: string;
-}
 @Component({
   selector: 'app-user',
   standalone: true,
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
+  imports: [CardComponent]
 })
 export class UserComponent {
   @Input({required:true}) user!: User; 
-  @Output() select = new EventEmitter<string>();
+  @Input({required:true}) selected!: boolean;
+  @Output() select = new EventEmitter();
   get imagePath() {
     return 'assets/users/' + this.user.avatar;
   }
